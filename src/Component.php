40b6fc5729a074b19f03645fc64d4d55ebd1feb4
egidio.pignataro <egidio.pignataro@gruppomol.it>
2025-03-13 11:58:34 +0100

    better docs for component

--- src/Component.php
+++ src/Component.php
@@ -3,16 +3,34 @@
 namespace DumpsterfireComponents;
 
 use DumpsterfireBase\Container\Container;
+use DumpsterfireComponents\Exceptions\ComponentRendererException;
 use DumpsterfireComponents\Interfaces\ISetup;
 use DumpsterfireComponents\Renderer\ComponentRenderer;
 
+/**
+ * Base Component class.
+ * Extend this class to create your own component.
+ */
 abstract class Component
 {
+    protected bool $enabled = true;
+
+    /**
+     * Prints the HTML content of a component.
+     * @return void
+     * @throws ComponentRendererException
+     */
     public function render(): void
     {
         echo $this->content();
     }
 
+    /**
+     * Returns the HTML content of a component.
+     *
+     * @return string
+     * @throws Exceptions\ComponentRendererException
+     */
     public function content(): string
     {
         $this->preRender();
@@ -20,6 +38,46 @@ abstract class Component
         return Container::getInstance()->create(ComponentRenderer::class)->loadComponent($this)->getHtmlContent();
     }
 
+    /**
+     * Disables a component: its content will be empty.
+     *
+     * @return $this
+     */
+    public function disable(): self
+    {
+        $this->enabled = false;
+
+        return $this;
+    }
+
+    /**
+     * Enables a component.
+     *
+     * @return $this
+     */
+    public function enable(): self
+    {
+        $this->enabled = true;
+
+        return $this;
+    }
+
+    /**
+     * Returns true if the component is enabled.
+     *
+     * @return bool
+     */
+    public function isEnabled(): bool
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * Runs the setup method if the component implements the ISetup interface.
+     * May be subject to modifications.
+     *
+     * @return void
+     */
     private function preRender(): void
     {
         if ($this instanceof ISetup) {
